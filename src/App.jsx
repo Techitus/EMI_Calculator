74d7f7cae70610df5543af00e7b34afe7785034c
Kamal Paudel <techitus07@gmail.com>
2024-05-15 17:01:47 +0545

    some js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 import BasicPie from "./piechart"
 
 
 const App = () => {
-
+  const [amount,setAmount] = useState()
+  const [interest, setInterest] = useState()
+  const [loan,setLoan] = useState()
   return (
   <>
   <div className="wrapper bg-green-400 h-screen w-screen absolute">
@@ -12,31 +15,31 @@ const App = () => {
       <div className="rangeSlide  w-96 ml-4 p-4 flex ">
         <div className="">
         <label htmlFor="medium-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Loan Amount</label>
-<input id="medium-range" type="range" value="10" className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
+<input id="medium-range" min={0.0} max={10000000.0} type="range" value={amount} onChange={(e)=>setAmount(e.target.value)}className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
         </div>
       
 <div className="">
-<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" placeholder="Rs."  />
+<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" value={amount} placeholder="Rs."  />
 </div>
       </div>
       <div className="rangeSlide  w-96 ml-4 p-4 flex ">
         <div className="">
         <label htmlFor="medium-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Interest Rate</label>
-        <input id="medium-range" type="range" value="50" className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
+        <input id="medium-range" min={1.0} max={20.0} value={interest} onChange={(e)=>setInterest(e.target.value)} type="range"  className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
         </div>
       
 <div className="">
-<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" placeholder="p.a"  />
+<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" placeholder="p.a" value={interest}  />
 </div>
       </div>
       <div className="rangeSlide  w-96 ml-4 p-4 flex ">
         <div className="">
         <label htmlFor="medium-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Loan Term</label>
-        <input id="medium-range" type="range" value="50" className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
+        <input id="medium-range" min={1} max={20}  type="range" value={loan} onChange={(e)=>setLoan(e.target.value)} className="w-full h-2 mb-6 rounded-lg accent-green-600  cursor-pointer "/>
         </div>
       
 <div className="">
-<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" placeholder="years"  />
+<input className="w-32 mt-5 ml-3 bg-gray-300" type="text" name="" id="" placeholder="years"  value={loan} />
 </div>
       </div>
       </div>
